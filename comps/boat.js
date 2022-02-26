352596a020163b6f7a30f51586909a3c995216e8
mrlowbot <karwanazar@icloud.com>
2022-02-26 07:16:48 +0100

    1.0.4

--- comps/boat.js
+++ comps/boat.js
@@ -4,6 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { loadGLTFModel } from '../lib/model'
 import { DogSpinner, DogContainer } from './boat-loader'
 import src from '../public/Wood_baseColor.png'
+import { Th } from '@chakra-ui/react'
 
 function easeOutCirc(x) {
   return Math.sqrt(1 - Math.pow(x - 1, 4))
@@ -77,7 +78,7 @@ const VoxelDog = () => {
 
       loadGLTFModel(scene, '/scene.gltf', {
         receiveShadow: false,
-        castShadow: false,
+        castShadow: false
       }).then(() => {
         animate()
         setLoading(false)
