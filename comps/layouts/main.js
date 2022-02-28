4ecca6bc20947a2f3efc2d3c2424c31f2d99a217
mrlowbot <karwanazar@icloud.com>
2022-02-28 16:17:33 +0100

    1.0.7

--- comps/layouts/main.js
+++ comps/layouts/main.js
@@ -15,7 +15,7 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="SOT" />
+        <meta name="description" content="Philosophy" />
         <meta name="author" content="Karwan M.A." />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
